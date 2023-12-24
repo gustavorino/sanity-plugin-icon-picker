327f16a85608a3433ae35cecd87a2db324a67193
Gustavo Bremm <gustavorino@gmail.com>
2023-12-24 06:30:07 -0300

    remove tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,1 @@
 import '@testing-library/jest-dom/extend-expect';
-import { setupGlobalMatchMedia, setupGlobalResizeObserver } from './test/mocks';
-
-setupGlobalMatchMedia();
-setupGlobalResizeObserver();
